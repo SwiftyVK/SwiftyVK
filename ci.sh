d929d2cd124b6ef7ac79c962f5ace3eb7d6db935
Alexey Kudryavtsev <westor@me.com>
2016-05-24 23:11:25 +0300

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -2,5 +2,5 @@
 
 set -e
 
-xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-OSX" test CODE_SIGNING_REQUIRED=NO
-xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-iOS" -destination "platform=iOS Simulator,name=iPhone 6" test CODE_SIGNING_REQUIRED=NO
+xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-OSX" test CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY=""
+xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-iOS" -destination "platform=iOS Simulator,name=iPhone 6" test CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY=""
