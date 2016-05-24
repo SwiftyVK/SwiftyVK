f76e62979c110b23aa65c7f9b33395da9fce4df3
Alexey Kudryavtsev <westor@me.com>
2016-05-24 20:18:54 +0300

    Update ci.sh

--- ci.sh
+++ ci.sh
@@ -4,4 +4,4 @@ set -e
 
 #xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-iOS" -destination "platform=iOS Simulator,name=iPhone 6" test
 
-xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-OSX" test
+xcodebuild -workspace SwiftyVK.xcworkspace -list
