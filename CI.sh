67ebde2d230e180ecf571a6da8b22aa58a4765da
Alexey Kudryavtsev <westor@me.com>
2016-08-05 18:46:22 +0300

    Delete CI.sh

--- CI.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-OSX" test CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY=""
-xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-iOS" -destination "platform=iOS Simulator,name=iPhone 6" test CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY=""
