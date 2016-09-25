d915b39eceb46fbc230b7df8fad046a60366a5a0
Alexey Kudryavtsev <westor@me.com>
2016-09-25 17:52:36 +0300

    remove code signing for travis

--- .travis.sh
+++ .travis.sh
@@ -1,2 +1,2 @@
-xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-OSX" test
-xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-iOS" -destination "platform=iOS Simulator,name=iPhone 7" test
+xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-OSX" CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" test
+xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-iOS" -destination "platform=iOS Simulator,name=iPhone 7" CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" test
