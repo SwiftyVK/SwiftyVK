dac3d308bd5e4a932f8b56ffa04be3a703a62af3
Alexey Kudryavtsev <westor@me.com>
2016-09-25 18:29:14 +0300

    build-for-testing

--- .travis.sh
+++ .travis.sh
@@ -1,2 +1,2 @@
-xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-OSX" CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" test-without-building
-xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-iOS" -destination "platform=iOS Simulator,name=iPhone 7" CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" test-without-building
+xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-OSX" CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" build-for-testing test-without-building
+xcodebuild -workspace SwiftyVK.xcworkspace -scheme "SwiftyVK-Example-iOS" -destination "platform=iOS Simulator,name=iPhone 7" CODE_SIGNING_REQUIRED=NO CODE_SIGN_IDENTITY="" build-for-testing test-without-building
