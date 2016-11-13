e8a07c412325c2a42399bdd036b3a75817a835e7
Alexey Kudryavtsev <westor@me.com>
2016-11-13 15:05:43 +0300

    [CocoaPods] fix multiplatform build

--- Library/Sources/SDK/UI/CaptchaController-OSX.swift
+++ Library/Sources/SDK/UI/CaptchaController-OSX.swift
@@ -1,78 +1,80 @@
-import Cocoa
-
-
-
-private let captchaViewName = Resources.withSuffix("CaptchaView")
-
-
-
-class CaptchaController: NSWindowController, NSTextFieldDelegate {
-
-    @IBOutlet private weak var imageView: NSImageView!
-    @IBOutlet private weak var textField: NSTextField!
-    private var parentWindow: NSWindow?
-    private weak var delegate: CaptchaPresenter!
-    private var image: NSImage?
-
-
-    static func create(data: Data, delegate: CaptchaPresenter) -> CaptchaController? {
-
-        guard let image = NSImage(data: data) else {
-            return nil
-        }
-
-        let controller = CaptchaController()
-        controller.parentWindow = VK.delegate?.vkWillPresentView()
-        controller.image = image
-        controller.delegate = delegate
-
-        return DispatchQueue.main.sync {
-            NSNib(nibNamed: captchaViewName, bundle: Resources.bundle)?.instantiate(withOwner: controller, topLevelObjects: nil)
+#if os(macOS)
+    import Cocoa
+    
+    
+    
+    private let captchaViewName = Resources.withSuffix("CaptchaView")
+    
+    
+    
+    class CaptchaController: NSWindowController, NSTextFieldDelegate {
+        
+        @IBOutlet private weak var imageView: NSImageView!
+        @IBOutlet private weak var textField: NSTextField!
+        private var parentWindow: NSWindow?
+        private weak var delegate: CaptchaPresenter!
+        private var image: NSImage?
+        
+        
+        static func create(data: Data, delegate: CaptchaPresenter) -> CaptchaController? {
             
-            guard let window = controller.window else {
+            guard let image = NSImage(data: data) else {
                 return nil
             }
             
-            _ = controller.parentWindow != nil
-                ? controller.parentWindow?.beginSheet(window, completionHandler: nil)
-                : controller.showWindow(nil)
+            let controller = CaptchaController()
+            controller.parentWindow = VK.delegate?.vkWillPresentView()
+            controller.image = image
+            controller.delegate = delegate
             
-            return controller
+            return DispatchQueue.main.sync {
+                NSNib(nibNamed: captchaViewName, bundle: Resources.bundle)?.instantiate(withOwner: controller, topLevelObjects: nil)
+                
+                guard let window = controller.window else {
+                    return nil
+                }
+                
+                _ = controller.parentWindow != nil
+                    ? controller.parentWindow?.beginSheet(window, completionHandler: nil)
+                    : controller.showWindow(nil)
+                
+                return controller
+            }
         }
-    }
-
-
-
-    func setText(to text: String) {
-        textField.stringValue = text
-    }
-
-
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-
-        window?.styleMask.formUnion(NSFullSizeContentViewWindowMask)
-        window?.titleVisibility = .hidden
-        window?.titlebarAppearsTransparent = true
-        imageView.image = image
-        textField.delegate = self
-
-        DispatchQueue.global(qos: .background).async {
-            Thread.sleep(forTimeInterval: 0.5)
-            self.delegate.didAppear()
+        
+        
+        
+        func setText(to text: String) {
+            textField.stringValue = text
         }
-    }
-
-
-
-
-    override func controlTextDidEndEditing(_ obj: Notification) {
-        if let window = window {
-            parentWindow?.endSheet(window)
-            window.orderOut(parentWindow)
+        
+        
+        
+        override func awakeFromNib() {
+            super.awakeFromNib()
+            
+            window?.styleMask.formUnion(NSFullSizeContentViewWindowMask)
+            window?.titleVisibility = .hidden
+            window?.titlebarAppearsTransparent = true
+            imageView.image = image
+            textField.delegate = self
+            
+            DispatchQueue.global(qos: .background).async {
+                Thread.sleep(forTimeInterval: 0.5)
+                self.delegate.didAppear()
+            }
         }
-        delegate.finish(answer: textField.stringValue)
+        
+        
+        
+        
+        override func controlTextDidEndEditing(_ obj: Notification) {
+            if let window = window {
+                parentWindow?.endSheet(window)
+                window.orderOut(parentWindow)
+            }
+            delegate.finish(answer: textField.stringValue)
+        }
+        
     }
-
-}
+#endif
--- Library/Sources/SDK/UI/WebController-iOS.swift
+++ Library/Sources/SDK/UI/WebController-iOS.swift
@@ -1,103 +1,105 @@
-import UIKit
-import WebKit
-
-
-
-private let webViewName = Resources.withSuffix("WebView")
-
-
-
-internal final class WebController: UIViewController, UIWebViewDelegate {
-
-    @IBOutlet private weak var webView: UIWebView?
-    @IBOutlet private weak var activity: UIActivityIndicatorView!
-    fileprivate var parentView: UIViewController?
-    private weak var delegate: WebPresenter!
-    private var url: String?
-
-
-
-    class func create(withDelegate delegate: WebPresenter) -> WebController? {
-
-        guard let parentView = VK.delegate?.vkWillPresentView() else {
-            return nil
+#if os(iOS)
+    import UIKit
+    import WebKit
+    
+    
+    
+    private let webViewName = Resources.withSuffix("WebView")
+    
+    
+    
+    internal final class WebController: UIViewController, UIWebViewDelegate {
+        
+        @IBOutlet private weak var webView: UIWebView?
+        @IBOutlet private weak var activity: UIActivityIndicatorView!
+        fileprivate var parentView: UIViewController?
+        private weak var delegate: WebPresenter!
+        private var url: String?
+        
+        
+        
+        class func create(withDelegate delegate: WebPresenter) -> WebController? {
+            
+            guard let parentView = VK.delegate?.vkWillPresentView() else {
+                return nil
+            }
+            
+            var controller: WebController!
+            
+            DispatchQueue.main.sync {
+                controller              = WebController(nibName: webViewName, bundle: Resources.bundle)
+                controller.delegate     = delegate
+                controller.parentView   = parentView
+                controller.modalPresentationStyle = .overFullScreen
+                controller.modalTransitionStyle = .crossDissolve
+                controller.parentView?.present(controller, animated: true, completion: nil)
+            }
+            
+            return controller
         }
-
-        var controller: WebController!
-
-        DispatchQueue.main.sync {
-            controller              = WebController(nibName: webViewName, bundle: Resources.bundle)
-            controller.delegate     = delegate
-            controller.parentView   = parentView
-            controller.modalPresentationStyle = .overFullScreen
-            controller.modalTransitionStyle = .crossDissolve
-            controller.parentView?.present(controller, animated: true, completion: nil)
+        
+        
+        
+        override func viewDidLoad() {
+            webView?.delegate = self
+            super.viewDidLoad()
         }
-
-        return controller
-    }
-
-
-
-    override func viewDidLoad() {
-        webView?.delegate = self
-        super.viewDidLoad()
-    }
-
-
-
-    override func viewDidDisappear(_ animated: Bool) {
-        delegate?.finish()
-        super.viewDidDisappear(animated)
-    }
-
-
-
-    func load(url: String) {
-        if self.url == nil {
-            self.url = url
+        
+        
+        
+        override func viewDidDisappear(_ animated: Bool) {
+            delegate?.finish()
+            super.viewDidDisappear(animated)
         }
-
-        guard let urlStr = self.url, let url = URL(string: urlStr) else {return}
-        VK.Log.put("WebController", "load \(urlStr)")
-
-        DispatchQueue.main.sync {
-            self.webView?.loadRequest(URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 3))
+        
+        
+        
+        func load(url: String) {
+            if self.url == nil {
+                self.url = url
+            }
+            
+            guard let urlStr = self.url, let url = URL(string: urlStr) else {return}
+            VK.Log.put("WebController", "load \(urlStr)")
+            
+            DispatchQueue.main.sync {
+                self.webView?.loadRequest(URLRequest(url: url, cachePolicy: .reloadIgnoringLocalCacheData, timeoutInterval: 3))
+            }
+        }
+        
+        
+        
+        func expand() {}
+        
+        
+        
+        func goBack() {
+            VK.Log.put("WebController", "goBack")
+            
+            webView?.goBack()
+        }
+        
+        
+        
+        func hide() {
+            VK.Log.put("WebController", "hide")
+            
+            self.parentView?.dismiss(animated: true, completion: nil)
+            self.webView?.delegate = nil
+        }
+        
+        
+        
+        //MARK: - frameLoadDelegate protocol
+        func webViewDidFinishLoad(_ webView: UIWebView) {
+            activity.stopAnimating()
+            delegate?.handleResponse(webView.request?.url?.absoluteString ?? "")
+        }
+        
+        
+        
+        func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
+            delegate?.handleError(.failedAuthorization)
         }
     }
-
-
-
-    func expand() {}
-
-
-
-    func goBack() {
-        VK.Log.put("WebController", "goBack")
-
-        webView?.goBack()
-    }
-
-
-
-    func hide() {
-        VK.Log.put("WebController", "hide")
-
-        self.parentView?.dismiss(animated: true, completion: nil)
-        self.webView?.delegate = nil
-    }
-
-
-
-    //MARK: - frameLoadDelegate protocol
-    func webViewDidFinishLoad(_ webView: UIWebView) {
-        activity.stopAnimating()
-        delegate?.handleResponse(webView.request?.url?.absoluteString ?? "")
-    }
-
-
-
-    func webView(_ webView: UIWebView, didFailLoadWithError error: Error) {
-        delegate?.handleError(.failedAuthorization)
-    }
-}
+#endif
