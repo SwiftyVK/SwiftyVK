087c2b19c9ee4b23d141e064c1d87f2c20837f34
Alexey Kudryavtsev <westor@me.com>
2017-03-28 20:53:42 +0300

    Add new SwiftLint rules

--- Library/Sources/SDK/Models/Errors.swift
+++ Library/Sources/SDK/Models/Errors.swift
@@ -75,7 +75,6 @@ public struct ApiError: CustomNSError, CustomStringConvertible {
 
 
 
-
 extension NSError {
     override open var description: String {
         return String(format: "error %@[%d]: %@", domain, code, userInfo[NSLocalizedDescriptionKey] as? String ?? "nil")
