873cf9547b9aef63924a2edcb483ead3ffad1172
Alexey Kudryavtsev <westor@me.com>
2017-03-30 20:51:39 +0300

    Add Market API methods

--- Library/Sources/API/Market.swift
+++ Library/Sources/API/Market.swift
@@ -1,125 +1,28 @@
-extension _VKAPI {
-  ///Methods for working with messages. More - https://vk.com/dev/messages
-  public struct Market {
-
-    ///More - https://vk.com/dev/market.get
-    public static func get(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.get", parameters: parameters)
+public extension Api {
+    public enum Market: String, Method {
+        case get = "market.get"
+        case getById = "market.getById"
+        case search = "market.search"
+        case getAlbums = "market.getAlbums"
+        case getAlbumById = "market.getAlbumById"
+        case createComment = "market.createComment"
+        case getComments = "market.getComments"
+        case deleteComment = "market.deleteComment"
+        case restoreComment = "market.restoreComment"
+        case editComment = "market.editComment"
+        case reportComment = "market.reportComment"
+        case getCategories = "market.getCategories"
+        case report = "market.report"
+        case add = "market.add"
+        case edit = "market.edit"
+        case delete = "market.delete"
+        case restore = "market.restore"
+        case reorderItems = "market.reorderItems"
+        case reorderAlbums = "market.reorderAlbums"
+        case addAlbum = "market.addAlbum"
+        case editAlbum = "market.editAlbum"
+        case deleteAlbum = "market.deleteAlbum"
+        case removeFromAlbum = "market.removeFromAlbum"
+        case addToAlbum = "market.addToAlbum"
     }
-
-    ///More - https://vk.com/dev/market.getById
-    public static func getById(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.getById", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.search
-    public static func search(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.search", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.getAlbums
-    public static func getAlbums(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.getAlbums", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.getAlbumById
-    public static func getAlbumById(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.getAlbumById", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.createComment
-    public static func createComment(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.createComment", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.getComments
-    public static func getComments(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.getComments", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.deleteComment
-    public static func deleteComment(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.deleteComment", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.restoreComment
-    public static func restoreComment(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.restoreComment", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.editComment
-    public static func editComment(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.editComment", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.reportComment
-    public static func reportComment(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.reportComment", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.getCategories
-    public static func getCategories(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.getCategories", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.report
-    public static func report(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.report", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.add
-    public static func add(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.add", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.edit
-    public static func edit(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.edit", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.delete
-    public static func delete(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.delete", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.restore
-    public static func restore(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.restore", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.reorderItems
-    public static func reorderItems(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.reorderItems", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.reorderAlbums
-    public static func reorderAlbums(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.reorderAlbums", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.addAlbum
-    public static func addAlbum(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.addAlbum", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.editAlbum
-    public static func editAlbum(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.editAlbum", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/messages.get
-    public static func deleteAlbum(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.deleteAlbum", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.removeFromAlbum
-    public static func removeFromAlbum(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.removeFromAlbum", parameters: parameters)
-    }
-
-    ///More - https://vk.com/dev/market.addToAlbum
-    public static func addToAlbum(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "market.addToAlbum", parameters: parameters)
-    }
-  }
 }
