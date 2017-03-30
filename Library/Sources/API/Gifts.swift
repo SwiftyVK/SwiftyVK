2b724faf38d86070dda2c1afb4fddcb1335dfc0e
Alexey Kudryavtsev <westor@me.com>
2017-03-30 21:21:24 +0300

    Add Gifts API methods

--- Library/Sources/API/Gifts.swift
+++ Library/Sources/API/Gifts.swift
@@ -1,10 +1,5 @@
-extension _VKAPI {
-  ///Methods for working with gifts. More - https://vk.com/dev/gifts
-  public struct Gifts {
-
-    ///Returns list of user gifts. More - https://vk.com/dev/gifts.get
-    public static func get(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "get", parameters: parameters)
+public extension Api {
+    public enum Gifts: String, Method {
+        case get = "gifts.get"
     }
-  }
 }
