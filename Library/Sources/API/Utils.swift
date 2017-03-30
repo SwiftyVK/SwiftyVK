46289610d9fd425f2055cc1be285cf25c822ea1a
Alexey Kudryavtsev <westor@me.com>
2017-03-30 21:12:39 +0300

    Add Utils API methods

--- Library/Sources/API/Utils.swift
+++ Library/Sources/API/Utils.swift
@@ -1,20 +1,7 @@
-extension _VKAPI {
-  ///Utilites. More - https://vk.com/dev/
-  public struct Utils {
-
-    ///Checks whether a link is blocked in VK. More - https://vk.com/dev/utils.checkLink
-    public static func checkLink(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "utils.checkLink", parameters: parameters)
+public extension Api {
+    public enum Utils: String, Method {
+        case checkLink = "utils.checkLink"
+        case resolveScreenName = "utils.resolveScreenName"
+        case getServerTime = "utils.getServerTime"
     }
-
-    ///Detects a type of object (e.g., user, community, application) and its ID by screen name. More - https://vk.com/dev/utils.resolveScreenName
-    public static func resolveScreenName(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "utils.resolveScreenName", parameters: parameters)
-    }
-
-    ///Returns the current time of the VK server. More - https://vk.com/dev/utils.getServerTime
-    public static func getServerTime(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "utils.getServerTime", parameters: parameters)
-    }
-  }
 }
