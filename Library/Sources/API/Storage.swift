920997d839c9a33137835ba72194ecac597bf0eb
Alexey Kudryavtsev <westor@me.com>
2017-03-30 21:10:50 +0300

    Add Storage API methods

--- Library/Sources/API/Storage.swift
+++ Library/Sources/API/Storage.swift
@@ -1,20 +1,7 @@
-extension _VKAPI {
-  ///Methods for working with storage. More - https://vk.com/dev/storage
-  public struct Storage {
-
-    ///Returns a value of variable with the name set by key parameter. More - http://vk.com/dev/storage.get
-    public static func get(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "storage.get", parameters: parameters)
+public extension Api {
+    public enum Storage: String, Method {
+        case get = "storage.get"
+        case set = "storage.set"
+        case getKeys = "storage.getKeys"
     }
-
-    ///Saves a value of variable with the name set by key parameter. More - https://vk.com/dev/storage.set
-    public static func set(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "storage.set", parameters: parameters)
-    }
-
-    ///Returns the names of all variables. More - https://vk.com/dev/storage.getKeys
-    public static func getKeys(_ parameters: [VK.Arg : String] = [:]) -> RequestConfig {
-      return RequestConfig(method: "storage.getKeys", parameters: parameters)
-    }
-  }
 }
