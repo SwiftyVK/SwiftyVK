15bc23ccdb3062f0be2b799f5cac100cef85e661
Alexey Kudryavtsev <westor@me.com>
2017-04-04 22:40:54 +0300

    Make Result as enum

--- Library/Sources/SDK/Request/Result.swift
+++ Library/Sources/SDK/Request/Result.swift
@@ -1,66 +1,47 @@
 import Foundation
 
-internal struct Result: CustomStringConvertible {
-    private unowned let request: RequestInstance
+enum Result: CustomStringConvertible {
 
-    internal private(set) var error: Error? {
-        didSet {
-            error != nil && response != nil ? response = nil : ()
-            guard let error = error else {return}
-            VK.Log.put(request, "result contained \(error)")
-        }
-    }
+    case data(JSON)
+    case error(Error)
     
-    internal private(set) var response: JSON? {
-        didSet {error != nil && response != nil ? error = nil : ()}
-    }
-
-    init(request: RequestInstance) {
-        self.request = request
-    }
-
-    internal mutating func parseResponseFrom(data: Data) {
-            let parsingError: NSErrorPointer = nil
-            var json = JSON(data: data, error: parsingError)
-
-            if let parsingError = parsingError?.pointee {
-                VK.Log.put(request, "json not parsed")
-                error = parsingError
-            }
-            else if json["response"].exists() {
-                VK.Log.put(request, "result contained response")
-                response = json["response"]
-            }
-            else if json["error"].exists() {
-                error = ApiError(json: json["error"])
-            }
-            else if !json.isEmpty {
-                VK.Log.put(request, "result contained response")
-                response = json
-            }
-            else {
-                VK.Log.put(request, "json not contain response or error")
-                error = RequestError.responseParsingFailed
-            }
+    init(from data: Data) {
+        let parsingError: NSErrorPointer = nil
+        var json = JSON(data: data, error: parsingError)
+        
+        if let parsingError = parsingError?.pointee {
+//            VK.Log.put(request, "json not parsed")
+            self = .error(parsingError)
+        }
+        else if json["error"].exists() {
+            self = .error(ApiError(json: json["error"]))
+        }
+        else if json["response"].exists() {
+//            VK.Log.put(request, "result contained response")
+            self = .data(json["response"])
+        }
+        else if !json.isEmpty {
+//            VK.Log.put(request, "result contained response")
+            self = .data(json)
+        }
+        else {
+//            VK.Log.put(request, "json not contain response or error")
+            self = .error(RequestError.responseParsingFailed)
+        }
     }
 
     @discardableResult
     internal mutating func setError(error: Error) -> Error {
-        self.error = error
+        self = .error(error)
         return error
     }
 
     var description: String {
-        if response != nil {
+        switch self {
+        case .data:
             return "result with response"
-        }
-        else if let error = error as NSError? {
+        case .error(let error):
             return "result with: \(error)"
         }
-        else if let error = error {
-            return "result with: \(error)"
-        }
-
-        return "result empty"
     }
 }
