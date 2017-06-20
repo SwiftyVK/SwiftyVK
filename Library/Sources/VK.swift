fa96e5b2d36d52465fc15c3ea147173c20d4cd31
Alexey Kudryavtsev <westor@me.com>
2017-06-20 20:51:45 +0300

    Remove LegacyVKDelegate

--- Library/Sources/VK.swift
+++ Library/Sources/VK.swift
@@ -7,37 +7,6 @@ import Foundation
     public typealias VkViewController = NSViewController
 #endif
 
-///Delegate to the SwiftyVK
-public protocol LegacyVKDelegate: class {
-    /**Called when SwiftyVK need autorization permissions
-     - returns: permissions as VK.Scope type*/
-    func vkWillAuthorize() -> Set<VK.Scope>
-    ///Called when SwiftyVK did authorize and receive token
-    func vkDidAuthorizeWith(parameters: [String : String])
-    ///Called when SwiftyVK did unauthorize and remove token
-    func vkDidUnauthorize()
-    ///Called when SwiftyVK did failed autorization
-    func vkAutorizationFailedWith(error: SessionError)
-    #if os(iOS)
-    /**Called when need to display a view from SwiftyVK
-     - returns: UIViewController that should present autorization view controller*/
-    func vkWillPresentView() -> UIViewController
-    #elseif os(OSX)
-    /**Called when need to display a view from SwiftyVK
-     - returns: Parent window for modal view or nil if view should present in separate window.*/
-    func vkWillPresentView() -> NSWindow?
-    #endif
-}
-//
-//
-//
-//
-//
-//
-//
-//
-//
-//
 public final class VK {
     
     public struct Api {
@@ -64,6 +33,4 @@ public final class VK {
         dependencyHolder?.authorizator.handle(url: url, app: app)
     }
     #endif
-    
-    weak static var legacyDelegate: LegacyVKDelegate?
 }
