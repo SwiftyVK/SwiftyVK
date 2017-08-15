cebb62acf8e71fd345dcc99e29065e58d72c80ea
Alexey Kudryavtsev <westor@me.com>
2017-08-15 21:26:16 +0300

    Remove Errors.swift

--- Library/Sources/SDK/Models/Errors.swift
+++
@@ -1,7 +0,0 @@
-import Foundation
-
-extension NSError {
-    override open var description: String {
-        return String(format: "error %@[%d]: %@", domain, code, userInfo[NSLocalizedDescriptionKey] as? String ?? "nil")
-    }
-}
