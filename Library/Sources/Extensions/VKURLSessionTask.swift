500466e21038826fd9ebe166b082bcb6fac313ee
Alexey Kudryavtsev <westor@me.com>
2017-08-21 21:37:37 +0300

    Fix vars and methods sequencing

--- Library/Sources/Extensions/VKURLSessionTask.swift
+++ Library/Sources/Extensions/VKURLSessionTask.swift
@@ -1,13 +1,13 @@
 import Foundation
 
 protocol VKURLSessionTask: ObservableObject {
-    func resume()
-    func cancel()
-    
     var countOfBytesReceived: Int64 { get }
     var countOfBytesSent: Int64 { get }
     var countOfBytesExpectedToSend: Int64 { get }
     var countOfBytesExpectedToReceive: Int64 { get }
+    
+    func resume()
+    func cancel()
 }
 
 extension URLSessionTask: VKURLSessionTask {}
