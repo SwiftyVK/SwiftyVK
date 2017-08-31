04d1ef0f0025e345e54332cd3cda6fb2be3f5706
Alexey Kudryavtsev <westor@me.com>
2017-08-31 18:26:26 +0300

    Conform CustomMethod to Method protocol

--- Library/Sources/API/Custom.swift
+++ Library/Sources/API/Custom.swift
@@ -1,31 +1,50 @@
 public extension VKAPI {
     public struct Custom {
-        public static func method(name: String, parameters: Parameters = .empty) -> CustomMethod {
-            return CustomMethod(method: name, parameters: parameters)
-        }
-        
         public static func remote(method: String) -> CustomMethod {
             return self.method(name: "execute.\(method)")
         }
         
-        public static func execute(code: String, config: Config = .default) -> Request {
-            return CustomMethod(method: "execute", parameters: [.code: code]).request(with: config)
+        public static func method(name: String, parameters: Parameters = .empty) -> CustomMethod {
+            return CustomMethod(method: name, parameters: parameters)
+        }
+        
+        public static func execute(code: String, config: Config = .default) -> CustomMethod {
+            return CustomMethod(method: "execute", parameters: [.code: code])
         }
     }
 }
 
 // Do not use this class directly. Use Api.Custom
-public class CustomMethod {
-    public let method: String
-    public let parameters: Parameters
+public final class CustomMethod {
+    let method: String
+    let parameters: Parameters
     
     init(method: String, parameters: Parameters = .empty) {
         self.method = method
         self.parameters = parameters
     }
-    
-    func request(with config: Config = .default) -> Request {
-        return Request(type: .api(method: method, parameters: parameters))
+}
+
+extension CustomMethod: Method {
+    public func configure(with config: Config) -> SuccessableFailbaleMethod {
+        let request = toRequest()
+        request.config = config
+        return SuccessableFailbaleMethod(request)
     }
     
+    public func onSuccess(_ clousure: @escaping (Data) -> ()) -> FailableConfigurableMethod {
+        let request = toRequest()
+        request.callbacks.onSuccess = clousure
+        return FailableConfigurableMethod(request)
+    }
+    
+    public func onError(_ clousure: @escaping (VKError) -> ()) -> SuccessableConfigurableMethod {
+        let request = toRequest()
+        request.callbacks.onError = clousure
+        return SuccessableConfigurableMethod(request)
+    }
+    
+    public func toRequest() -> Request {
+        return Request(type: .api(method: method, parameters: parameters))
+    }
 }
