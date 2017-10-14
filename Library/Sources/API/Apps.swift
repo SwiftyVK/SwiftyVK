b258cb06c44fb4cfa8e3c333e524feaa46d85c7a
Alexey Kudryavtsev <westor@me.com>
2017-10-14 16:45:39 +0300

    Add apps methods group

---
+++ Library/Sources/API/Apps.swift
@@ -0,0 +1,11 @@
+public extension PrivateVKAPI {
+    public enum Apps: APIMethod {
+        case deleteAppRequests(Parameters)
+        case get(Parameters)
+        case getCatalog(Parameters)
+        case getFriendsList(Parameters)
+        case getLeaderboard(Parameters)
+        case getScore(Parameters)
+        case sendRequest(Parameters)
+    }
+}
