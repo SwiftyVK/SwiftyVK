4cf889ab6694f82e271a8820301122bf77f5d6d8
Alexey Kudryavtsev <westor@me.com>
2017-10-14 16:53:53 +0300

    Update Docs methods group

--- Library/Sources/API/Docs.swift
+++ Library/Sources/API/Docs.swift
@@ -1,11 +1,15 @@
 public extension PrivateVKAPI {
     public enum Docs: APIMethod {
+        case add(Parameters)
+        case delete(Parameters)
+        case edit(Parameters)
         case get(Parameters)
         case getById(Parameters)
+        case getMessagesUploadServer(Parameters)
+        case getTypes(Parameters)
         case getUploadServer(Parameters)
         case getWallUploadServer(Parameters)
         case save(Parameters)
-        case delete(Parameters)
-        case add(Parameters)
+        case search(Parameters)
     }
 }
