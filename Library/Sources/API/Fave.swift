0524afa9dd0fd498ffa9d42e2ac351069b1a2ec1
Alexey Kudryavtsev <westor@me.com>
2017-10-14 16:55:45 +0300

    Update Fave methods group

--- Library/Sources/API/Fave.swift
+++ Library/Sources/API/Fave.swift
@@ -1,16 +1,16 @@
 public extension PrivateVKAPI {
     public enum Fave: APIMethod {
-        case getUsers(Parameters)
+        case addGroup(Parameters)
+        case addLink(Parameters)
+        case addUser(Parameters)
+        case getLinks(Parameters)
+        case getMarketItems(Parameters)
         case getPhotos(Parameters)
         case getPosts(Parameters)
+        case getUsers(Parameters)
         case getVideos(Parameters)
-        case getLinks(Parameters)
-        case addUser(Parameters)
-        case removeUser(Parameters)
-        case addGroup(Parameters)
         case removeGroup(Parameters)
-        case addLink(Parameters)
         case removeLink(Parameters)
-        case getMarketItems(Parameters)
+        case removeUser(Parameters)
     }
 }
