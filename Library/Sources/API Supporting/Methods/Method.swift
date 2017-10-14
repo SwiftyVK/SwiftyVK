693f98a306f33438e7baf4ca50de2120ee534606
Alexey Kudryavtsev <westor@me.com>
2017-10-14 23:03:45 +0300

    Fix method interface

--- Library/Sources/API Supporting/Methods/Method.swift
+++ Library/Sources/API Supporting/Methods/Method.swift
@@ -5,7 +5,7 @@ public protocol SendableMethod {
 extension SendableMethod {
     
     @discardableResult
-    func send() -> Task {
+    public func send() -> Task {
         guard let session = VK.sessions?.default else {
             fatalError("You must call VK.setUp function to start using SwiftyVK!")
         }
@@ -24,25 +24,25 @@ public protocol ChainableMethod: SendableMethod {}
 public protocol Method: ChainableMethod {}
 
 extension Method {
-    func onSuccess(_ clousure: @escaping RequestCallbacks.Success) -> Methods.FailableConfigurable {
+    public func onSuccess(_ clousure: @escaping RequestCallbacks.Success) -> Methods.FailableConfigurable {
         let request = toRequest()
         request.callbacks.onSuccess = clousure
         return .init(request)
     }
     
-    func onError(_ clousure: @escaping RequestCallbacks.Error) -> Methods.SuccessableConfigurable {
+    public func onError(_ clousure: @escaping RequestCallbacks.Error) -> Methods.SuccessableConfigurable {
         let request = toRequest()
         request.callbacks.onError = clousure
         return .init(request)
     }
     
-    func configure(with config: Config) -> Methods.SuccessableFailable {
+    public func configure(with config: Config) -> Methods.SuccessableFailable {
         let request = toRequest()
         request.config = config
         return .init(request)
     }
     
-    func chain(_ next: @escaping (Data) throws -> ChainableMethod) -> Methods.SuccessableFailableConfigurable {
+    public func chain(_ next: @escaping (Data) throws -> ChainableMethod) -> Methods.SuccessableFailableConfigurable {
         let request = toRequest()
         request.add(next: next)
         return .init(request)
