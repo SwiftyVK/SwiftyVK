ced1716bacd7b489c38e3d2c876bc52518e32684
Alexey Kudryavtsev <westor@me.com>
2017-10-14 17:26:03 +0300

    Update Pages methods group

--- Library/Sources/API/Pages.swift
+++ Library/Sources/API/Pages.swift
@@ -1,12 +1,12 @@
 public extension PrivateVKAPI {
     public enum Pages: APIMethod {
+        case clearCache(Parameters)
         case get(Parameters)
-        case save(Parameters)
-        case saveAccess(Parameters)
         case getHistory(Parameters)
         case getTitles(Parameters)
         case getVersion(Parameters)
         case parseWiki(Parameters)
-        case clearCache(Parameters)
+        case save(Parameters)
+        case saveAccess(Parameters)
     }
 }
