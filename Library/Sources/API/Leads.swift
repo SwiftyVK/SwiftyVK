2eb15cb260d6defde1697a94c4cf21e3bf4dd0d0
Alexey Kudryavtsev <westor@me.com>
2017-10-14 17:09:50 +0300

    Add Leads methods group

---
+++ Library/Sources/API/Leads.swift
@@ -0,0 +1,10 @@
+public extension PrivateVKAPI {
+    public enum Leads: APIMethod {
+        case checkUser(Parameters)
+        case complete(Parameters)
+        case getStats(Parameters)
+        case getUsers(Parameters)
+        case metricHit(Parameters)
+        case start(Parameters)
+    }
+}
