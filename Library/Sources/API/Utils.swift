97ec82b0e075e91b9271a977820059ec41982504
Alexey Kudryavtsev <westor@me.com>
2017-10-14 17:43:42 +0300

    Update Utils methods group

--- Library/Sources/API/Utils.swift
+++ Library/Sources/API/Utils.swift
@@ -1,7 +1,11 @@
 public extension PrivateVKAPI {
     public enum Utils: APIMethod {
         case checkLink(Parameters)
-        case resolveScreenName(Parameters)
+        case deleteFromLastShortened(Parameters)
+        case getLastShortenedLinks(Parameters)
+        case getLinkStats(Parameters)
         case getServerTime(Parameters)
+        case getShortLink(Parameters)
+        case resolveScreenName(Parameters)
     }
 }
