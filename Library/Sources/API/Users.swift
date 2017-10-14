b24a89e9ea5abe4b8a0dd4e20791d2bd70ea6770
Alexey Kudryavtsev <westor@me.com>
2017-10-14 17:42:25 +0300

    Update Users methods group

--- Library/Sources/API/Users.swift
+++ Library/Sources/API/Users.swift
@@ -1,11 +1,11 @@
 public extension PrivateVKAPI {
     public enum Users: APIMethod {
         case get(Parameters)
-        case search(Parameters)
-        case isAppUser(Parameters)
-        case getSubscriptions(Parameters)
         case getFollowers(Parameters)
-        case report(Parameters)
         case getNearby(Parameters)
+        case getSubscriptions(Parameters)
+        case isAppUser(Parameters)
+        case report(Parameters)
+        case search(Parameters)
     }
 }
