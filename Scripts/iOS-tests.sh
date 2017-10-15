eda82e4392d1bb640f15b81acfd57d5b2df158f3
Alexey Kudryavtsev <westor@me.com>
2017-10-15 13:33:14 +0300

    [Travis] Remove scripts

--- Scripts/iOS-tests.sh
+++
@@ -1,9 +0,0 @@
-xcodebuild \
--workspace SwiftyVK.xcworkspace \
--scheme SwiftyVK_iOS \
--destination "platform=iOS Simulator,name=iPhone 7 Plus" \
--enableCodeCoverage YES \
-CODE_SIGNING_REQUIRED=NO \
-CODE_SIGN_IDENTITY="" \
-test | xcpretty -f `xcpretty-travis-formatter`
-
--- Scripts/macOS-tests.sh
+++
@@ -1,8 +0,0 @@
-xcodebuild \
--workspace SwiftyVK.xcworkspace \
--scheme SwiftyVK_macOS \
--enableCodeCoverage YES \
-CODE_SIGNING_REQUIRED=NO \
-CODE_SIGN_IDENTITY="" \
-ENABLE_TESTABILITY=YES \
-test | xcpretty -f `xcpretty-travis-formatter`
