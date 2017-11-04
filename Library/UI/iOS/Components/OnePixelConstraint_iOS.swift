fe019c07ccd954c9513ee581bacf1c862930c7b2
Alexey Kudryavtsev <westor@me.com>
2017-11-04 12:16:39 +0300

    Implement OnePixel constraint for separators

---
+++ Library/UI/iOS/Components/OnePixelConstraint_iOS.swift
@@ -0,0 +1,9 @@
+import UIKit
+
+class OnePixelConstraintIOS: NSLayoutConstraint {
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        constant =  1 / UIScreen.main.scale
+    }
+}
