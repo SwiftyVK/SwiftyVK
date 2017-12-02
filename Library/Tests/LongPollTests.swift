8bf268d607fa6c6b9215ae842d5deebc07df31b9
Alexey Kudryavtsev <westor@me.com>
2017-12-02 14:03:28 +0300

    Fix warnings in tests

--- Library/Tests/LongPollTests.swift
+++ Library/Tests/LongPollTests.swift
@@ -284,7 +284,7 @@ final class LongPollTests: XCTestCase {
         context.longPoll.start { events in
             if taskCount == 1 {
                 guard case .forcedStop? = events.first else {
-                    return XCTFail("Unexpected event: \(events.first)")
+                    return XCTFail("Unexpected events: \(events)")
                 }
                 
                 expectation.fulfill()
