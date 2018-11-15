0e2288637cfbe6be1d92aa55cbb80aec68628e11
Alexey Kudryavtsev <westor@me.com>
2018-11-15 09:36:36 +0300

    [SwiftLint] Fix warnings

--- Example/AppDelegate.swift
+++ Example/AppDelegate.swift
@@ -12,19 +12,31 @@ var vkDelegateReference : SwiftyVKDelegate?
     final class AppDelegate : UIResponder, UIApplicationDelegate {
         var window: UIWindow?
         
-        func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+        func application(
+            _ application: UIApplication,
+            didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil
+        ) -> Bool {
             vkDelegateReference = VKDelegateExample()
             return true
         }
         
         @available(iOS 9.0, *)
-        func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
+        func application(
+            _ app: UIApplication,
+            open url: URL,
+            options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+        ) -> Bool {
             let app = options[.sourceApplication] as? String
             VK.handle(url: url, sourceApplication: app)
             return true
         }
         
-        func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: Any) -> Bool {
+        func application(
+            _ application: UIApplication,
+            open url: URL,
+            sourceApplication: String?,
+            annotation: Any
+        ) -> Bool {
             VK.handle(url: url, sourceApplication: sourceApplication)
             return true
         }
