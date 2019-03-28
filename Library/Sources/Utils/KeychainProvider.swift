a73142c0470fa70323e57f3953968385aaf8a24b
Alexey Kudryavtsev <westor@me.com>
2019-03-28 20:59:48 +0300

    Migrate to Xcode 10.2

--- Library/Sources/Utils/KeychainProvider.swift
+++ Library/Sources/Utils/KeychainProvider.swift
@@ -11,10 +11,23 @@ class KeychainProvider<EntityType> {
     func save(_ entity: EntityType, for sessionId: String) throws {
         let keychainQuery = keychainParamsFor(sessionId: sessionId)
         
-        keychainQuery.setObject(
-            NSKeyedArchiver.archivedData(withRootObject: entity),
-            forKey: NSString(format: kSecValueData)
-        )
+        let data: Data
+        
+        if #available(iOS 11.0, OSX 10.13, *) {
+            data = try NSKeyedArchiver.archivedData(withRootObject: entity, requiringSecureCoding: false)
+        }
+        else if #available(iOS 2.0, OSX 10.11, *) {
+            data = NSKeyedArchiver.archivedData(withRootObject: entity)
+        }
+        else {  
+            // Because Xcode 10 has a bug - http://openradar.appspot.com/49262697
+            // swiftlint:disable next compiler_protocol_init
+            let selector = Selector(stringLiteral: "archivedDataWithRootObject:")
+            // swiftlint:disable next force_cast force_unwrapping
+            data = NSKeyedArchiver.perform(selector, with: entity)!.takeRetainedValue() as! Data
+        }
+
+        keychainQuery.setObject(data, forKey: NSString(format: kSecValueData))
         
         removeFor(sessionId: sessionId)
         
