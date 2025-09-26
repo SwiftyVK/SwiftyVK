1be52a13cbdc53f799a8cf6c315c099b289053f9
Alexey Kudryavtsev <westor@me.com>
2025-09-26 12:21:32 +0300

    Fix WebView for login, #189

---
+++ Library/UI/WebViewUserAgent.swift
@@ -0,0 +1,5 @@
+import Foundation
+
+enum WebViewUserAgent {
+    static let mobileSafari = "Mozilla/5.0 (iPhone; CPU iPhone OS 16_0 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Mobile/15E148"
+}
