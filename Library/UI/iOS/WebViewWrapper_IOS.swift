600c4ed71bf09be49819b13c0a0dad3a8e839d77
Aleksei Khokhrin <aleksey.a.ho@gmail.com>
2025-10-25 17:51:21 +0300

    issue #189 fix: The login page on the iPad is glitchy

--- Library/UI/iOS/WebViewWrapper_IOS.swift
+++ Library/UI/iOS/WebViewWrapper_IOS.swift
@@ -6,6 +6,7 @@ final class WebViewWrapperIOS: UIView {
     
     public required init?(coder: NSCoder) {
         webView = WKWebView(frame: .zero, configuration: WKWebViewConfiguration())
+        webView.customUserAgent = "Mozilla/5.0 (iPhone; CPU iPhone OS 17_5 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/17.0 Mobile/15E148 Safari/604.1"
         super.init(coder: coder)
         addSubview(webView)
     }
