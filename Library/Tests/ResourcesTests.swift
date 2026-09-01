7a9b0d63efa1e9ea69c2e0a8a6f6b7981836e4d3
Alexey Kudryavtsev <westor@me.com>
2026-09-01 12:26:22 +0300

    fix(ui): correct storyboard runtime attributes

--- Library/Tests/ResourcesTests.swift
+++ Library/Tests/ResourcesTests.swift
@@ -2,8 +2,12 @@ import Foundation
 import XCTest
 @testable import SwiftyVK
 
+#if os(iOS)
+import UIKit
+#endif
+
 final class ResourcesTests: XCTestCase {
-    
+
     func test_suffixForPlatform() {
         // When
         let path = Resources.withSuffix("test")
@@ -18,4 +22,41 @@ final class ResourcesTests: XCTestCase {
             XCTAssertEqual(path, "test_watchOS")
         #endif
     }
+
+    #if os(iOS)
+    func test_extendedInsetsButton_containsPointOutsideBounds() {
+        // Given
+        let button = ExtendedInsetsButtonIOS(frame: CGRect(x: 0, y: 0, width: 22, height: 22))
+        button.extendedInsets = 50
+        // When
+        let result = button.point(inside: CGPoint(x: -8, y: -8), with: nil)
+        // Then
+        XCTAssertTrue(result)
+    }
+
+    func test_extendedInsetsButton_doesNotContainPointOutsideBounds_whenInsetsAreZero() {
+        // Given
+        let button = ExtendedInsetsButtonIOS(frame: CGRect(x: 0, y: 0, width: 22, height: 22))
+        // When
+        let result = button.point(inside: CGPoint(x: -8, y: -8), with: nil)
+        // Then
+        XCTAssertFalse(result)
+    }
+
+    func test_captchaStoryboard_configuresExtendedInsetsButton() {
+        // Given
+        let storyboard = UIStoryboard(name: Resources.withSuffix("Storyboard"), bundle: Resources.bundle)
+        let controller = storyboard.instantiateViewController(withIdentifier: "Captcha")
+        controller.loadViewIfNeeded()
+        // When
+        let button = buttons(in: controller.view).first as? ExtendedInsetsButtonIOS
+        // Then
+        XCTAssertEqual(button?.extendedInsets, 50)
+    }
+
+    private func buttons(in view: UIView) -> [UIButton] {
+        let button = (view as? UIButton).map { [$0] } ?? []
+        return button + view.subviews.flatMap(buttons(in:))
+    }
+    #endif
 }
